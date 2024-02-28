dd7fea8e680742209b3e640a2e27890ba3b761d7
PavlosKalivas <kalivas_pau@hotmail.com>
2024-02-28 19:32:10 +0200

    final

--- lab1.py
+++ lab1.py
@@ -1,30 +1,35 @@
 import requests  # εισαγωγή της βιβλιοθήκης
 import re
+import datetime
 
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
+# URL χρήστη
+url = input ("Please give me the url you want (e.g. http://google.com ) : ")
+try :
+    # Αίτημα HTTP στο URL του χρήστη
+    with requests.get(url) as response: 
+        #Headers της απόκρισης HTTP
+        print(f"\nThe Headers for this URL < {url}> are : \n\n  {response.headers} \n\n")
 
-url = 'http://google.com/'  # προσδιορισμός του url
+        ## τροποποίηση
 
-with requests.get(url) as response:  # το αντικείμενο response
-    #html = response.text
-    #more(html)
-    print(f"Headers are {url} \n , {response.headers} \n\n")
-    server= response.headers.get("Server")
-
-    if server :
-        print(f"The Server is : {server}")
-    else:
-        print("no server found")
-    
-    cookies = response.cookies.get_dict()
-    print(f"The Cookies are : {cookies}")
-
-    cookie_header = response.headers.get("Set-Cookie")
+        #server
+        server= response.headers.get("Server")
+        
+        if server :
+            print(f"Server : {server}")
+        else:
+            print("no server has been found")
+        
+        # cookies and exp date
+        cookies = response.cookies
+        if cookies :
+            for cookie in cookies:
+                print(f"Cookies <name=value> : {cookie.name} = {cookie.value}")
+                #print("Cookie Value:", cookie.value)
+                try:
+                    print("Cookie Exp date:", datetime.datetime.fromtimestamp(cookie.expires).strftime('%d-%m-%Y'))
+                except :
+                    print("No date has been found")
+        else :
+            print("No Cookies has been found")
+except : print("Wrong URL ... Bye...")
